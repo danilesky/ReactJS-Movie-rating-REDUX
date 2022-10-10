72fdb2ffda6247c24676622549a8f81ffe106785
Daniel Skysľak <danilesky17@gmail.com>
2022-10-10 21:10:32 +0200

    cleanup Redux core method

--- src/state/store.js
+++ src/state/store.js
@@ -1,10 +1,1 @@
-import { combineReducers, createStore } from "redux";
-import { moviesReducer } from "./movies.reducer";
-import { ratingReducer } from "./rating.reducer";
-
-export const combinedReducers = combineReducers({
-  movies: moviesReducer,
-  ratings: ratingReducer,
-});
-
-export const store = createStore(combinedReducers);
+import { configureStore } from "@reduxjs/toolkit";
