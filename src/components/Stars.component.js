24e29535c3f242db73b2ee15443f01f6ea621afe
Daniel Skysľak <danilesky17@gmail.com>
2022-10-10 20:44:09 +0200

    hover stars

--- src/components/Stars.component.js
+++ src/components/Stars.component.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaRegStar } from "react-icons/fa";
 import { useDispatch } from "react-redux";
+import styled from "styled-components";
+
+const Star = styled(FaRegStar)`
+  font-size: 30px;
+  cursor: pointer;
+  background: ${({ theme }) => (theme.hovered ? theme.hovered : "")};
+`;
 
 const Stars = ({ movie }) => {
+  const [hover, setHover] = useState(-1);
   const dispatch = useDispatch();
+
+  const hoverHandler = (index) => {
+    setHover(index);
+  };
   const addRating = (rate) => {
     dispatch({
       type: "DELETE_MOVIE",
@@ -16,11 +28,14 @@ const Stars = ({ movie }) => {
   };
   return (
     <div>
-      <FaRegStar onClick={() => addRating(1)} />
-      <FaRegStar onClick={() => addRating(2)} />
-      <FaRegStar onClick={() => addRating(3)} />
-      <FaRegStar onClick={() => addRating(4)} />
-      <FaRegStar onClick={() => addRating(5)} />
+      {[...Array(5)].map((item, index) => (
+        <Star
+          onMouseEnter={() => hoverHandler(index)}
+          theme={index <= hover ? { hovered: "red" } : ""}
+          onClick={() => addRating(index + 1)}
+          onMouseLeave={() => setHover(-1)}
+        />
+      ))}
     </div>
   );
 };
